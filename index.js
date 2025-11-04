1cba0af34f356597be9f3d6f8b87765000e95511
Yanis NAÎT-CHERIF <yanis.nait-cherif@interlogement93.net>
2025-11-04 14:51:42 +0100

    change table

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const pool = new Pool({
 });
 
 // Exemple endpoint
-app.get("/users", async (req, res) => {
+app.get("/menage", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM menage"); // Mets ton nom de table ici
     res.json(result.rows);
